1f8a2b8ac45092887dad051623a9ac9a0079118c
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2023-10-31 19:26:38 -0500

    Add constructor

--- Chess/src/Piece.java
+++ Chess/src/Piece.java
@@ -1,4 +1,9 @@
 public class Piece {
+    Piece(String pawnType, String color, boolean isAlive) {
+        this.pawnType = pawnType;
+        this.color = color;
+        this.isAlive = isAlive;
+    }
     // Type
     // Chess pieces
         // Queen
@@ -7,6 +12,7 @@ public class Piece {
         // Bishop
         // Knight
         // Pawn
+        //- pawn should have properties that allow promotion
     boolean isGraveyard;
 }
 
