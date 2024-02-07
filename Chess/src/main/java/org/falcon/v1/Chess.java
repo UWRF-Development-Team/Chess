44c3db025b346e381020aad78337dd1c991d1df6
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-02-06 18:33:24 -0600

    Add method for getting user piece choice

--- Chess/src/main/java/org/falcon/v1/Chess.java
+++ Chess/src/main/java/org/falcon/v1/Chess.java
@@ -38,6 +38,41 @@ public class Chess {
         }
         return input;
     }
+    public static PieceChar getPieceChoice() {
+        System.out.println("""
+                Pick a piece:
+                1. Pawn
+                2. Rook
+                3. Horse
+                4. Bishop
+                5. King
+                6.Queen""");
+        Scanner userInput = new Scanner(System.in);
+        int userChoice = 0;
+        boolean validInput = false;
+        PieceChar piece = null;
+        while(!validInput) {
+            try {
+                userChoice = userInput.nextInt();
+                if (userChoice >= 1 && userChoice <= 6) {
+                    piece = switch (userChoice) {
+                        case 1 -> PieceChar.PAWN;
+                        case 2 -> PieceChar.ROOK;
+                        case 3 -> PieceChar.HORSE;
+                        case 4 -> PieceChar.BISHOP;
+                        case 5 -> PieceChar.KING;
+                        case 6 -> PieceChar.QUEEN;
+                        default -> throw new IllegalStateException(("Unexpected value: " + userChoice));
+                    };
+                } else {
+                    System.out.println("Please enter a number between 1 and 6.");
+                }
+            } catch (InputMismatchException ex) {
+                System.err.println("Input an integer please.");
+            }
+        }
+        return piece;
+    }
     public void chooseContinuePlaying() {
         Scanner userInput = new Scanner(System.in);
         boolean validInput = false;
