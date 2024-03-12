e51e4cbae10b28dc1f259ba913a2f7e8beb36556
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-03-12 13:37:09 -0500

    Refactor play game method

--- Chess/src/main/java/org/falcon/v1/Chess.java
+++ Chess/src/main/java/org/falcon/v1/Chess.java
@@ -7,19 +7,78 @@ import java.util.Scanner;
 public class Chess {
     // 2 Players
     Board board;
+
     boolean continuePlaying;
     public Chess() {
         this.continuePlaying = true;
         this.board = new Board();
+
     }
+
+
+//    public void playGame() {
+//        while (this.continuePlaying) {
+//            this.board.printBoard();
+//            int row = getValidIntInput("Choose row");
+//            int col = getValidIntInput("Choose column");
+//            char piece = this.board.choosePiece();
+//            this.board.placePiece(row, col, piece);
+//            this.chooseContinuePlaying();
+//        }
+//    }
     public void playGame() {
         while (this.continuePlaying) {
             this.board.printBoard();
-            int row = getValidIntInput("Choose row");
-            int col = getValidIntInput("Choose column");
-            char piece = this.board.choosePiece();
-            this.board.placePiece(row, col, piece);
+
+            this.board.moveForward(this.getSpot());
             this.chooseContinuePlaying();
+        }
+    }
+
+    public BoardSpot getSpot() {
+        boolean isValidSpot = false;
+        int row = 0;
+        int col = 0;
+        while (!isValidSpot) {
+            row = this.getCoordinate("Enter the row number (1-8): ");
+            col = this.getCoordinate("Enter the column number (1-8): ");
+            boolean isCoordinateEmpty = this.isCoordinateEmpty(row, col);
+            if (isCoordinateEmpty) {
+                isValidSpot = false;
+                System.out.println("This is an empty space, try again!");
+            } else {
+                isValidSpot = true;
+            }
+        }
+        return new BoardSpot(row, col);
+    }
+    public int getCoordinate(String prompt) {
+        System.out.println(prompt);
+        Scanner input = new Scanner(System.in);
+        boolean isValidSpot = false;
+        int coordinate = 0;
+        while (!isValidSpot) {
+            try {
+                coordinate = input.nextInt();
+                if (coordinate < 1 || coordinate > 8) {
+                    isValidSpot = false;
+                } else {
+                    isValidSpot = true;
+                }
+            } catch (InputMismatchException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        return coordinate;
+    }
+    // TODO: Move to board class
+    public boolean isCoordinateEmpty(int row, int col) {
+        BoardSpot spot = new BoardSpot(row, col);
+        char pieceAtSpot = this.board.getPieceAtSpot(spot);
+        if (pieceAtSpot == PieceChar.EMPTY.getPieceChar()) {
+            return true;
+        } else {
+            return false;
         }
     }
     public static int getValidIntInput(String prompt) {
@@ -74,10 +133,11 @@ public class Chess {
         return piece;
     }
     public void chooseContinuePlaying() {
+        System.out.println("Do you want to continue playing? (y/n)");
         Scanner userInput = new Scanner(System.in);
         boolean validInput = false;
         while (!validInput) {
-            String input = userInput.nextLine();
+            String input = userInput.nextLine().toLowerCase();
             if (input.equals("y") || input.equals("yes")) {
                 this.continuePlaying = true;
                 validInput = true;
