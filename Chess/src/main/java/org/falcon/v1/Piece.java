acf6afc8714f9285917eda9aac61203f8b259ac8
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-03-12 13:38:58 -0500

    Refactor to include movement in each piece

--- Chess/src/main/java/org/falcon/v1/Piece.java
+++ Chess/src/main/java/org/falcon/v1/Piece.java
@@ -1,14 +1,17 @@
 package org.falcon.v1;
 
+import java.util.ArrayList;
+
 public abstract class Piece {
     Movement movement;
-    Piece() {
-        
+    Piece(Movement movement) {
+        this.movement = movement;
     }
-    public abstract void losePiece();
-    public abstract void promotePiece();
-    public abstract void makeMove(int rowIndex, int colIndex);
-    public abstract boolean validMove(int rowIndex, int colIndex);
+    public abstract ArrayList<BoardSpot> possibleMoves();
+    //public abstract void losePiece();
+    //public abstract void promotePiece();
+    //public abstract void makeMove(int rowIndex, int colIndex);
+    //public abstract boolean validMove(int rowIndex, int colIndex);
     // Type
     // Chess pieces
         // Queen
