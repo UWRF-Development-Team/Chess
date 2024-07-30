ae066251c1637f852e018accdc9511af30cc11bf
theoliverlear <sigwartholiver2@gmail.com>
2024-07-30 18:14:29 -0500

    Move Horse.java to org.falcon.model.piece package and define a SpecialMovement

---
+++ Chess/src/main/java/org/falcon/model/piece/Horse.java
@@ -0,0 +1,7 @@
+package org.falcon.model.piece;
+
+import org.falcon.model.piece.movement.SpecialMovement;
+
+public class Horse {
+    public final SpecialMovement MOVEMENT = new SpecialMovement(3, 3, 3, 3, PieceChar.HORSE);
+}
