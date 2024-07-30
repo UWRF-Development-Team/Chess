d7b03467da817f4d7939c66bd2d4c2f66f5abb33
theoliverlear <sigwartholiver2@gmail.com>
2024-07-30 18:14:56 -0500

    Move King.java to org.falcon.model.piece package and define a Movement

---
+++ Chess/src/main/java/org/falcon/model/piece/King.java
@@ -0,0 +1,10 @@
+package org.falcon.model.piece;
+
+import org.falcon.model.piece.movement.Movement;
+
+public class King {
+    public final Movement MOVEMENT = new Movement(1, 1, 1, 1);
+    public King() {
+
+    }
+}
