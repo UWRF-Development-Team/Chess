ed4de71c99211b4ba7c51a4256759c0a47bb640c
theoliverlear <sigwartholiver2@gmail.com>
2024-07-30 18:12:22 -0500

    Rename and refactor BoardTestV2.java to BoardTest.java, update package and imports

--- Chess/src/test/java/junit/model/board/BoardTest.java
+++ Chess/src/test/java/junit/model/board/BoardTest.java
@@ -1,13 +1,13 @@
-package tests;
+package junit.model.board;
 
-import org.falcon.v1.Board;
+import org.falcon.model.board.Board;
 import org.junit.Test;
 
 import java.util.Arrays;
 
 import static org.junit.Assert.assertTrue;
 
-public class BoardTestV2 {
+public class BoardTest {
         Board testDefaultBoard = new Board();
     /*
 -----------------
@@ -34,7 +34,17 @@ public class BoardTestV2 {
                 {'R', 'H', 'B', 'K', 'Q', 'B', 'H', 'R'}
         };
         char[][] actualBoard = this.testDefaultBoard.getBoard();
+        printCharBoard(actualBoard);
+        printCharBoard(expectedBoard);
         boolean boardsEqual = Arrays.deepEquals(expectedBoard, actualBoard);
         assertTrue(boardsEqual);
+    }
+    public static void printCharBoard(char[][] board) {
+        int numRows = board.length;
+        int numCols = board[0].length;
+        System.out.println("Rows: " + numRows + " Cols: " + numCols);
+        for (char[] row : board) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 }
