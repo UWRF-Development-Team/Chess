33cca348254a6df045fa0a00bb430a1226457784
theoliverlear <sigwartholiver2@gmail.com>
2024-08-06 18:17:47 -0500

    Add unit tests for Queen and Rook movements in MovementTest.java

--- Chess/src/test/java/junit/model/piece/movement/MovementTest.java
+++ Chess/src/test/java/junit/model/piece/movement/MovementTest.java
@@ -2,18 +2,44 @@ package junit.model.piece.movement;
 
 import org.falcon.model.board.BoardSpot;
 import org.falcon.model.piece.King;
+import org.falcon.model.piece.Queen;
+import org.falcon.model.piece.Rook;
+import org.falcon.model.piece.movement.Movement;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MovementTest {
+
+    @Test
+    public void testIsValidMovement() {
+        Movement movement = new Movement(1, 1, 1, 1);
+        boolean actualIsValidMovement = movement.isValidMovement(new BoardSpot(1,1), new BoardSpot(2,2));
+        assertTrue(actualIsValidMovement);
+        actualIsValidMovement = movement.isValidMovement(new BoardSpot(1,1), new BoardSpot(3,3));
+        assertFalse(actualIsValidMovement);
+    }
     @Test
     public void testIsValidMovementKing() {
         King king = new King();
-        boolean actualIsValidMovement = king.MOVEMENT.isValidMovement(new BoardSpot(1,1), new BoardSpot(2,2));
+        boolean actualIsValidMovement = king.getMovement().isValidMovement(new BoardSpot(1,1), new BoardSpot(2,2));
         assertTrue(actualIsValidMovement);
-        actualIsValidMovement = king.MOVEMENT.isValidMovement(new BoardSpot(1,1), new BoardSpot(3,3));
-        assertFalse(actualIsValidMovement); // TODO: Fix movement, currently fails
+        actualIsValidMovement = king.getMovement().isValidMovement(new BoardSpot(1,1), new BoardSpot(3,3));
+        assertFalse(actualIsValidMovement);
+    }
+    @Test
+    public void testIsValidMovementQueen() {
+        Queen queen = new Queen();
+        boolean actualIsValidMovement = queen.getMovement().isValidMovement(new BoardSpot(1,1), new BoardSpot(2,2));
+        assertTrue(actualIsValidMovement);
+    }
+    @Test
+    public void testIsValidMovementRook() {
+        Rook rook = new Rook();
+        boolean actualIsValidMovement = rook.getMovement().isValidMovement(new BoardSpot(1,1), new BoardSpot(1,8));
+        assertTrue(actualIsValidMovement);
+        actualIsValidMovement = rook.getMovement().isValidMovement(new BoardSpot(1,1), new BoardSpot(2,2));
+        assertFalse(actualIsValidMovement);
     }
 }
