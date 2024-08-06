c62126431c246a520580d9ecd2fa5b8d5160036b
theoliverlear <sigwartholiver2@gmail.com>
2024-08-06 18:18:34 -0500

    Add Lombok annotations, extend Piece class, and add SpecialMovement for Pawn.java

--- Chess/src/main/java/org/falcon/model/piece/Pawn.java
+++ Chess/src/main/java/org/falcon/model/piece/Pawn.java
@@ -1,5 +1,12 @@
 package org.falcon.model.piece;
 
-public class Pawn {
-    // TODO: Implement SpecialMovement for Pawn
+import lombok.Getter;
+import lombok.Setter;
+import org.falcon.model.piece.movement.Movement;
+import org.falcon.model.piece.movement.SpecialMovement;
+
+@Getter
+@Setter
+public class Pawn extends Piece {
+    private final SpecialMovement movement = new SpecialMovement(1, 1, 1, 1, this);
 }
