fde04e50c9f1f9a04dbd468701707ebdd121b346
theoliverlear <sigwartholiver2@gmail.com>
2024-08-13 19:13:16 -0500

    Add constructors and firstMove flag to Pawn.java to incorporate board context and track initial move status

--- Chess/src/main/java/org/falcon/model/piece/Pawn.java
+++ Chess/src/main/java/org/falcon/model/piece/Pawn.java
@@ -2,11 +2,27 @@ package org.falcon.model.piece;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.falcon.model.board.Board;
 import org.falcon.model.piece.movement.Movement;
 import org.falcon.model.piece.movement.SpecialMovement;
 
 @Getter
 @Setter
 public class Pawn extends Piece {
-    private final SpecialMovement movement = new SpecialMovement(1, 1, 1, 1, this);
+    private final SpecialMovement movement = new SpecialMovement(1, 0, 1, 1, this);
+    private boolean firstMove;
+    public Pawn() {
+        this(true);
+    }
+    public Pawn(boolean firstMove) {
+        this.firstMove = firstMove;
+    }
+    public Pawn(Board board) {
+        this();
+        this.movement.setBoard(board);
+    }
+    public Pawn(boolean firstMove, Board board) {
+        this(firstMove);
+        this.movement.setBoard(board);
+    }
 }
