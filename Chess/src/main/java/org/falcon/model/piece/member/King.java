7b02eb119852e9433480cc60a5329e27c0d80eb6
theoliverlear <sigwartholiver2@gmail.com>
2024-08-20 18:15:41 -0500

    Refactor King.java to member package and add constructor with Player parameter

--- Chess/src/main/java/org/falcon/model/piece/member/King.java
+++ Chess/src/main/java/org/falcon/model/piece/member/King.java
@@ -1,8 +1,10 @@
-package org.falcon.model.piece;
+package org.falcon.model.piece.member;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.falcon.model.piece.Piece;
 import org.falcon.model.piece.movement.Movement;
+import org.falcon.model.player.Player;
 
 @Getter
 @Setter
@@ -10,5 +12,8 @@ public class King extends Piece {
     final Movement movement = new Movement(1, 1, 1, 1);
     public King() {
 
+    }
+    public King(Player player) {
+        super(player);
     }
 }
