1dd3e2de77beca64c31149b1af766ca554f19b44
theoliverlear <sigwartholiver2@gmail.com>
2024-08-20 18:14:04 -0500

    Refactor Rook.java to member package and add constructor with Player parameter

--- Chess/src/main/java/org/falcon/model/piece/member/Rook.java
+++ Chess/src/main/java/org/falcon/model/piece/member/Rook.java
@@ -1,11 +1,16 @@
-package org.falcon.model.piece;
+package org.falcon.model.piece.member;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.falcon.model.piece.Piece;
 import org.falcon.model.piece.movement.Movement;
+import org.falcon.model.player.Player;
 
 @Getter
 @Setter
 public class Rook extends Piece {
     final Movement movement = new Movement(-1, -1, -1, 0);
+    public Rook(Player player) {
+        super(player);
+    }
 }
