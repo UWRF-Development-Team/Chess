be101fa83e51b911b64c6c97da4c68d62245392b
theoliverlear <sigwartholiver2@gmail.com>
2024-08-20 18:15:14 -0500

    Refactor Horse.java to member package and add constructor with Player parameter

--- Chess/src/main/java/org/falcon/model/piece/member/Horse.java
+++ Chess/src/main/java/org/falcon/model/piece/member/Horse.java
@@ -1,11 +1,16 @@
-package org.falcon.model.piece;
+package org.falcon.model.piece.member;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.falcon.model.piece.Piece;
 import org.falcon.model.piece.movement.SpecialMovement;
+import org.falcon.model.player.Player;
 
 @Getter
 @Setter
 public class Horse extends Piece {
     final SpecialMovement movement = new SpecialMovement(3, 3, 3, 3, this);
+    public Horse(Player player) {
+        super(player);
+    }
 }
