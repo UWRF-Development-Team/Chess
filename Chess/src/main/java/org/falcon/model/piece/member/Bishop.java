269036538ca72d28f18737e8c61c6fb4ecf1adc3
theoliverlear <sigwartholiver2@gmail.com>
2024-08-20 18:13:41 -0500

    Refactor Bishop.java to member package and add constructor with Player parameter

--- Chess/src/main/java/org/falcon/model/piece/member/Bishop.java
+++ Chess/src/main/java/org/falcon/model/piece/member/Bishop.java
@@ -1,11 +1,16 @@
-package org.falcon.model.piece;
+package org.falcon.model.piece.member;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.falcon.model.piece.Piece;
 import org.falcon.model.piece.movement.Movement;
+import org.falcon.model.player.Player;
 
 @Getter
 @Setter
 public class Bishop extends Piece {
     private final Movement movement = new Movement(0, 0, -1, -1);
+    public Bishop(Player player) {
+        super(player);
+    }
 }
