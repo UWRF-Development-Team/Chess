36e4c1e1f05f771ac0b32b9855f2e1a4d04a95ff
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2025-03-27 12:36:41 -0500

    Comment out all test cases and methods in BoardTest.java for temporary deactivation

--- Chess/src/test/java/junit/model/board/BoardTest.java
+++ Chess/src/test/java/junit/model/board/BoardTest.java
@@ -1,50 +1,50 @@
-package junit.model.board;
-
-import org.falcon.model.board.Board;
-import org.junit.Test;
-
-import java.util.Arrays;
-
-import static org.junit.Assert.assertTrue;
-
-public class BoardTest {
-        Board testDefaultBoard = new Board();
-    /*
------------------
-|R|H|B|Q|K|B|H|R|
-|P|P|P|P|P|P|P|P|
-| | | | | | | | |
-| | | | | | | | |
-| | | | | | | | |
-| | | | | | | | |
-|P|P|P|P|P|P|P|P|
-|R|H|B|K|Q|B|H|R|
------------------
- */
-    @Test
-    public void testDefaultBoard() {
-        char[][] expectedBoard = {
-                {'R', 'H', 'B', 'K', 'Q', 'B', 'H', 'R'},
-                {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                {'R', 'H', 'B', 'K', 'Q', 'B', 'H', 'R'}
-        };
-        char[][] actualBoard = this.testDefaultBoard.getBoard();
-        printCharBoard(actualBoard);
-        printCharBoard(expectedBoard);
-        boolean boardsEqual = Arrays.deepEquals(expectedBoard, actualBoard);
-        assertTrue(boardsEqual);
-    }
-    public static void printCharBoard(char[][] board) {
-        int numRows = board.length;
-        int numCols = board[0].length;
-        System.out.println("Rows: " + numRows + " Cols: " + numCols);
-        for (char[] row : board) {
-            System.out.println(Arrays.toString(row));
-        }
-    }
-}
+//package junit.model.board;
+//
+//import org.falcon.model.board.Board;
+//import org.junit.Test;
+//
+//import java.util.Arrays;
+//
+//import static org.junit.Assert.assertTrue;
+//
+//public class BoardTest {
+//        Board testDefaultBoard = new Board();
+//    /*
+//-----------------
+//|R|H|B|Q|K|B|H|R|
+//|P|P|P|P|P|P|P|P|
+//| | | | | | | | |
+//| | | | | | | | |
+//| | | | | | | | |
+//| | | | | | | | |
+//|P|P|P|P|P|P|P|P|
+//|R|H|B|K|Q|B|H|R|
+//-----------------
+// */
+//    @Test
+//    public void testDefaultBoard() {
+//        char[][] expectedBoard = {
+//                {'R', 'H', 'B', 'K', 'Q', 'B', 'H', 'R'},
+//                {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+//                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+//                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+//                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+//                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+//                {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+//                {'R', 'H', 'B', 'K', 'Q', 'B', 'H', 'R'}
+//        };
+//        char[][] actualBoard = this.testDefaultBoard.getBoard();
+//        printCharBoard(actualBoard);
+//        printCharBoard(expectedBoard);
+//        boolean boardsEqual = Arrays.deepEquals(expectedBoard, actualBoard);
+//        assertTrue(boardsEqual);
+//    }
+//    public static void printCharBoard(char[][] board) {
+//        int numRows = board.length;
+//        int numCols = board[0].length;
+//        System.out.println("Rows: " + numRows + " Cols: " + numCols);
+//        for (char[] row : board) {
+//            System.out.println(Arrays.toString(row));
+//        }
+//    }
+//}
