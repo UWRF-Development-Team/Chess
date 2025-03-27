5b0a7bbd547952235457296b52346971ef28a602
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2025-03-27 12:53:44 -0500

    Add no-argument constructor to Queen.java to support default initialization

--- Chess/src/main/java/org/falcon/model/piece/member/Queen.java
+++ Chess/src/main/java/org/falcon/model/piece/member/Queen.java
@@ -10,6 +10,7 @@ import org.falcon.model.player.Player;
 @Setter
 public class Queen extends Piece {
     final Movement movement = new Movement(-1, -1, -1, -1);
+    public Queen() {}
     public Queen(Player player) {
         super(player);
     }
