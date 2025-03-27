4b051f567e490f370264e455897f5820237e6d73
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2025-03-27 12:47:26 -0500

    Add `pieceOrientation` field to Pawn.java to track pawn's orientation

--- Chess/src/main/java/org/falcon/model/piece/member/Pawn.java
+++ Chess/src/main/java/org/falcon/model/piece/member/Pawn.java
@@ -6,18 +6,22 @@ import org.falcon.model.board.Board;
 import org.falcon.model.piece.Piece;
 import org.falcon.model.piece.movement.SpecialMovement;
 import org.falcon.model.player.Player;
+import org.falcon.model.player.PlayerChar;
 
 @Getter
 @Setter
 public class Pawn extends Piece {
     private final SpecialMovement movement = new SpecialMovement(1, 0, 1, 1, this);
     private boolean firstMove;
+    public PieceOrientation pieceOrientation;
+
     public Pawn() {
         this(true);
     }
     public Pawn(Player player) {
         super(player);
         this.firstMove = true;
+
     }
     public Pawn(boolean firstMove) {
         this.firstMove = firstMove;
