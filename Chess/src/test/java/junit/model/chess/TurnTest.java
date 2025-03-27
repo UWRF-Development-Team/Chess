0aade7ca1b8088cffa74e273e736e5364d07c8d6
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2025-03-27 12:52:14 -0500

    Add TurnTest.java to test the Turn model functionality

---
+++ Chess/src/test/java/junit/model/chess/TurnTest.java
@@ -0,0 +1,23 @@
+package junit.model.chess;
+import org.falcon.model.chess.Turn;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TurnTest {
+    //    private T
+//    @Test
+//            private Turn currentTurn;
+//            this.currentTurn = new Turn();
+//            System.out.println(currentTurn.makeTurn());
+//            System.out.println(currentTurn.makeTurn());
+//
+//            private
+    private Turn testTurn = new Turn();
+
+    @Test
+    public void testCurrentTurn() {
+        testTurn.makeTurn();
+        assertTrue(testTurn.CheckIfWhiteTurn());
+    }
+}
