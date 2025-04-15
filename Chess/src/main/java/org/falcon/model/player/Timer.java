526db9b62aa774d6da03e1d042757576d8a2230b
TimothyRF <timothy.furlong@my.uwrf.edu>
2025-04-15 18:32:59 -0500

    Changes by dev team on april 15th 2025

--- Chess/src/main/java/org/falcon/model/player/Timer.java
+++ Chess/src/main/java/org/falcon/model/player/Timer.java
@@ -3,9 +3,14 @@ package org.falcon.model.player;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * run the timer on the front end, tracked on the back end
+ */
 @Getter
 @Setter
 public class Timer {
+    // Total time
+    // Time remaining
     private int time_remaining;
 
     public Timer(int setTime){
@@ -14,7 +19,7 @@ public class Timer {
 
     public void Countdown (int time_remaining) throws InterruptedException {
         while (time_remaining > 0) {
-            wait(1000);
+
             this.time_remaining--;
         }
         // local date time - local date time idea?
--- Chess/src/main/resources/static/global.ts
+++ Chess/src/main/resources/static/global.ts
@@ -1,82 +1,7 @@
-// const grid: any = document.getElementsByClassName('row');
-//
-// console.log('script loaded');
-//
-// const royalPurple: string = '#912A91';
-// const black: string = '#000000'
-// const white: string = '#FFFFFF'
-// const softPink: string = '#E4AFE7';
-//
-// if (grid) {
-//     console.log('grid is found');
-// } else {
-//     console.log('grid is not found');
-// }
-//
-// Array.from(grid).forEach((row: any) => {
-//     console.log('loaded row');
-//     const cols = row.getElementsByClassName('col')
-//     Array.from(cols).forEach((coord: any)=> {
-//         console.log('loaded col');
-//
-//         coord.addEventListener('mouseup', mouseUp);
-//         coord.addEventListener('mousedown', mouseDown);
-//         coord.addEventListener('mouseover', hover);
-//         coord.addEventListener('mouseleave', mouseLeave);
-//     });
-// });
-//
-// let selectedRow: number = -1;
-// let selectedCol: number = -1;
-//
-// function textToNumber(text: string): number {
-//     switch (text) {
-//         case "one":
-//             return 1;
-//         case "two":
-//             return 2;
-//         case "three":
-//             return 3;
-//         case "four":
-//             return 4;
-//         case "five":
-//             return 5;
-//         case "six":
-//             return 6;
-//         case "seven":
-//             return 7;
-//         case "eight":
-//             return 8;
-//         default:
-//             throw new Error("Invalid Board Index");
-//     }
-// }
-//
-// function mouseUp(this: any) {
-//     this.backgroundColor = white;
-// }
-//
-// function mouseDown(this: any) {
-//     this.style.backgroundColor = royalPurple;
-// }
-//
-// function saveCoord(this: any) {
-//     const coordColClasses = this.classList;
-//
-// }
-//
-// function mouseLeave(this: any) {
-//     this.style.backgroundColor = white;
-// }
-//
-// function hover(this: any) {
-//     this.style.backgroundColor = softPink;
-// }
-// const test = 1;
-
 const grid: any = document.getElementsByClassName('row');
 
 console.log('script loaded');
+console.log('test');
 
 const royalPurple: string = '#912A91';
 const black: string = '#000000'
@@ -89,10 +14,22 @@ if (grid) {
     console.log('grid is not found');
 }
 
+let squaresNumbered = 0;
+let rowAdjustment: number = 0;
+
+function isBlackSquareIndex() {
+    return (squaresNumbered + rowAdjustment) % 2 === 0;
+}
+
 Array.from(grid).forEach((row: any) => {
     console.log('loaded row');
-    const cols = row.getElementsByClassName('col')
+    const cols = row.getElementsByClassName('col');
+
     Array.from(cols).forEach((coord: any)=> {
+        if (isBlackSquareIndex()) {
+            coord.style.backgroundColor = black;
+        }
+        squaresNumbered++;
         console.log('loaded col');
 
         coord.addEventListener('mouseup', mouseUp);
@@ -100,6 +37,7 @@ Array.from(grid).forEach((row: any) => {
         coord.addEventListener('mouseover', hover);
         coord.addEventListener('mouseleave', mouseLeave);
     });
+    rowAdjustment++;
 });
 
 let selectedRow: number = -1;
@@ -129,11 +67,11 @@ function textToNumber(text: string): number {
 }
 
 function mouseUp(this: any) {
-    this.backgroundColor = white;
+    // this.backgroundColor = white;
 }
 
 function mouseDown(this: any) {
-    this.style.backgroundColor = royalPurple;
+    // this.style.backgroundColor = royalPurple;
 }
 
 function saveCoord(this: any) {
@@ -142,10 +80,10 @@ function saveCoord(this: any) {
 }
 
 function mouseLeave(this: any) {
-    this.style.backgroundColor = white;
+    // this.style.backgroundColor = white;
 }
 
 function hover(this: any) {
-    this.style.backgroundColor = softPink;
+    // this.style.backgroundColor = softPink;
 }
-const test = 1;
+
